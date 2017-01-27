8122ef08c7cb52e130b8a3579db3bf19a08cfeac
Ashley Henry <ashley.henry@reedonline.co.uk>
2017-01-27 18:27:17 +0000

    addition of card and deck

---
+++ Cards/Objects/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cards.Objects
+{
+    public class Card
+    {
+        public enum CardSuit
+        {
+            Diamonds,
+            Spades,
+            Hearts,
+            Clubs
+        }
+
+        public enum CardFace
+        {
+            Ace,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }
+
+        public Card()
+        {
+            CardSuit suit;
+            CardFace face;
+        }
+
+    }
+}
---
+++ Cards/Objects/CardDeck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards.Objects
+{
+    class CardDeck
+    {
+        public List<Card> Deck;
+    }
+}
